9d9811c378a06cb35721cf71354defe53ba94b1e
Demos <i.petrov@white-soft.ru>
2016-10-05 21:59:41 +1000

    Initial commit

---
+++ Assets/Scripts/Connector.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class Connector : MonoBehaviour {
+
+	private byte[] data = new byte[1024];
+	private string input;
+	private string stringData;
+
+	private IPEndPoint endpoint;
+	private Socket server;
+
+	// Use this for initialization
+	void Start () {
+
+		endpoint = new IPEndPoint (IPAddress.Parse("192.168.1.38"), 3159);
+		server = new Socket (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+		try {
+			server.Connect(endpoint);
+			Debug.Log ("Connection established.");
+		} catch(SocketException ex) {
+			Debug.Log ("Unnable to connect.");
+			Debug.Log (ex.ToString());
+			return;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var recieved = server.Receive (data);
+		stringData = Encoding.UTF8.GetString (data, 0, recieved);
+		Debug.Log (stringData);
+	}
+}
---
+++ Assets/Scripts/PlayerConroller.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerConroller : MonoBehaviour {
+
+	public float speed;
+	public float turnSpeed;
+
+	// Use this for initialization
+//	void Start () {
+//	
+//	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		var inputX = Input.GetAxisRaw ("Horizontal");
+		var inputY = Input.GetAxisRaw ("Vertical");
+
+		if ((Mathf.Abs (inputX) + Mathf.Abs(inputY)) > 0) {
+			transform.position += new Vector3 (inputX, inputY, 0).normalized * Time.deltaTime * speed;
+		}
+
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
+	}
+}
